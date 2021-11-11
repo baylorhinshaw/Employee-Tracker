2b58ffbd76fce715b48589e7ba91ad7e3f3d339a
Baylor <baylorhinshaw21@gmail.com>
2021-11-10 21:36:59 -0500

    need to fix file structure

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
-const mysql = require("mysql2");
+const inquirer = require("inquirer");
 const cTable = require("console.table");
+const db = require("./index");
 
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: 'root',
-      database: 'teams_db'
-    },
-    console.log(`Connected to the teams_db database.`)
-  );
 
-// view departments
-const viewAllDepartments = () => {
-    db.query('SELECT * FROM department', (err, results) => {
-        return results;
+const questions = [{
+    type: 'list',
+    message: 'What would you like to do?',
+    name: 'startQuestion',
+    choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Done']
+}]
+
+const startQuestion = () => {
+    inquirer.prompt(questions)
+    .then((response) => {
+        const { startQuestion } = response
+        if (startQuestion === 'View all departments') {
+            db.viewAllDepartments();
+        } else if (startQuestion === 'View all roles') {
+
+        } else if (startQuestion === 'View all employees') {
+            
+        } else if (startQuestion === 'Add a department'){
+
+        } else if (startQuestion === 'Add a role') {
+
+        } else if (startQuestion === 'Add an employee') {
+
+        } else if (startQuestion === 'Update an employee role') {
+
+        } else {
+            console.log('You are all done.')
+        }
     });
 };
 
+startQuestion();
 
-// view all roles
-db.query('SELECT * FROM roles', (err, results) => {
-    console.table(results);
-});
+// function viewDepartments() {
+//     db.viewAllDepartments()
+//     .then(results => {
+//         console.table(results)
+//     })
+// };
 
+// ask inquirer questions
+//db.addEmployees(inquirerAnswers)
 
-// view all employees
-db.query('SELECT * FROM employee', (err, results) => {
-    console.table(results);
-});
-
-// create department
-function addEmployee(queryParams){
-db.query(('INSERT INTO employee ?,?,?,?', queryParams), (err, results) => {
-    console.table(results);
-});
-}
-// create a department
-// create a role
-// create an employee
-// change an employee role
-
-module.exports;
+// object destructuring
+// name { key1: "value", key2: "different"}
+// before destructuring
+// console.log(name.key1)
+// destructured:
+// const { key1, key2 } = name
+// console.log(key1)
